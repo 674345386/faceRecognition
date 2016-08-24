4865c069008eba4d656f849cac524342e30e81c3
CP <674345386@qq.com>
2016-08-24 14:33:51 +0800

    摄像动画类

---
+++ src/TimerAction.java
@@ -0,0 +1,79 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Set;
+
+import javax.swing.Timer;
+
+import org.bytedeco.javacv.CanvasFrame;
+
+
+public class TimerAction implements ActionListener{
+	private CanvasFrame canvasFrame;
+	private Timer timer;
+	private Graphics2D graphics2d;
+	private int width;
+	private int height;
+	private int count_for_rect=0;
+	private int delta =15;
+	/**
+	 * 构造函数 
+	 */
+	public TimerAction(CanvasFrame canvasFrame){
+		this.width=canvasFrame.getWidth();
+		this.height=canvasFrame.getHeight();
+		this.graphics2d= (Graphics2D) canvasFrame.getCanvas().getGraphics();
+		graphics2d.setColor(Color.WHITE);
+		this.canvasFrame=canvasFrame;
+	}
+	public void setTimer(Timer timer){
+		this.timer=timer;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int offset =delta *count_for_rect;
+		if(width-2*offset>=0 && height-2*offset>=0){
+			graphics2d.drawRoundRect(offset, offset, width-2*offset, height-2*offset,50,50);
+			count_for_rect++;
+			canvasFrame.repaint();
+		}
+		else {
+			timer.stop();
+			count_for_rect=0;
+		}
+	}
+//	private Graphics2D g;
+//	private CanvasFrame canvasFrame;
+//	private int width, height;
+//
+//	private int delta = 10;
+//	private int count = 0;
+//
+//	private Timer timer;
+//
+//	public void setTimer(Timer timer) {
+//		this.timer = timer;
+////		this.timer.setRepeats(false);
+//	}
+//
+//	public TimerAction(CanvasFrame canvasFrame) {
+//		this.g = (Graphics2D) canvasFrame.getCanvas().getGraphics();
+//		this.canvasFrame = canvasFrame;
+//		this.width = canvasFrame.getWidth();
+//		this.height = canvasFrame.getHeight();
+//	}
+//
+//	public void actionPerformed(ActionEvent e) {
+//		int offset = delta * count;
+//		if (width - offset >= offset && height - offset >= offset) {
+//			g.draw3DRect(offset, offset, width - 2 * offset, height - 2 * offset,false);
+//			canvasFrame.repaint();
+//			count++;
+//		} else {
+//			// when animation is done, reset count and stop timer.
+//			timer.stop();
+//			count = 0;
+//		}
+//	}
+}
